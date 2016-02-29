9da0425b627a0473f5f7aed9d972d1b5ac1a9683
vincewong22 <vincewong22@gmail.com>
2016-02-29 13:45:12 -0800

    check 2

--- a2.c
+++ a2.c
@@ -7,6 +7,7 @@
 void print_number(int*, int*);
 void replace_char(char arg1, char arg2);
 void expand(char*);
+void output(char *set1, char *set2);
 
 int main(int argc, char* argv[]){
 if(argc != 3){
@@ -21,21 +22,53 @@ strcpy(expand2,argv[2]);
 
 size_t i;
 
-printf("expand 1 %s\n",expand1);
-printf("expand 2 %s\n",expand2);
+/* printf("expand 1 %s\n",expand1);
+printf("expand 2 %s\n",expand2); */
 
 expand(expand1);
+expand(expand2);
+
+output(expand1,expand2);
 
 return 0;
 }
 
+void output(char *set1, char *set2){
+
+int c;
+size_t i;
+size_t flag =0;
+int c_print;
+int truncate=0;
+
+/* printf("%c\n",set1[0]);
+printf("%c\n",set1[1]);
+printf("%c\n",set2[0]);
+printf("%c\n",set2[1]); */
+/* for(i=0; set1[i] !='\0'; i++)
+	printf("%c\n",set1[i]);
+for(i=0; set2[i] !='\0'; i++)
+	printf("%c\n",set2[i]); */
+/* printf("%c\n",set2[1]); */
+if(strlen(set2) > strlen(set1))
+	set2[strlen(set1)] = '\0';
+if(strlen(set1) > strlen(set2))
+	set1[strlen(set2)] = '\0';
+while((c=getchar()) != EOF){
+	c_print = c;
+	 for(i = 0; i<2; i++){
+		if(c == set1[i]){
+			c_print = set2[i];
+		}	
+	}
+	putchar(c_print);
+}
+}
 void print_number(int *five, int *six){
 printf("%d %d", *five, *six);
 }
 
 void expand(char *e){
-	
-printf("inside expand\n");
 size_t i, j;
 char temp[MAXSIZE];
 
@@ -92,23 +125,11 @@ for(i = 0, j=0; e[i] !='\0'; i++,j++){
 }
 	
 	temp[j] = '\0';
+for(i=0; temp[i] !='\0'; i++)
+	e[i] = temp[i];
 
-/* for(i = 0, j=0; e[i] !='\0'; i++,j++){
-	if(e[i] == '-'){ 
-	if(e[i-1] != '\0')
-		if(e[i] < e[i+2]) 
-	} */
-
-
-for(i=0; i < temp[i] !='\0'; i++)
-	printf("%c",temp[i]);
-
-i =0;
-printf("%c",e[i]);
-printf("%c",e[i-1]);
-printf("%c",e[i-2]);
-printf("%d",i);
 }
+
 
 
 
